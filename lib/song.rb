4e9b899903e7739120ca49f71980b6c26a948d72
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-28 16:17:52 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,51 @@
 class Song
-  attr_accessor :name, :artist_name
-  @@all = []
+  attr_accessor :name, :artist_name #
+  @@all = [] #
+  
+  def initialize
+    
+    self
+    @@all << self 
+    
+  end 
+  
+  def self.create
+    song = self.new
+  end 
+  
+  def self.new_by_name(name)
 
-  def self.all
-    @@all
+  end
+  
+  def self.create_by_name
+    
   end
 
-  def save
-    self.class.all << self
+  def self.find_by_name(name)
+    @@all.detect{ |songs| puts songs }
   end
+  
+  def self.find_or_create_by_name
+  end
+  
+  def self.alphabetical
+  end
+  
+  def self.new_from_file_name
+  end 
+  
+  def self.create_from_filename
+  
+  def self.destroy_all
+    @@all = []
+  end 
+  
+  def self.all   #
+    @@all        #
+  end            #
+                 #
+  def save       #
+    self.class.all << self #
+  end           #
 
 end
